44340958ecfd59c64546f5d95186351d64fa04f3
Sunil Kumar Maurya <sunilkumar.maurya@team.telstra.com>
2021-01-03 10:29:32 +0530

    example: express middleware to do some thing (verifying token/logging user activity etc) before reaching request to router

--- NodeJSApps/Mongodb-Task-Manager/src/index.js
+++ NodeJSApps/Mongodb-Task-Manager/src/index.js
@@ -5,10 +5,35 @@ const userRouter = require('../src/router/user')
 const taskRouter = require('../src/router/task')
 
 const port = process.env.PORT || 3000
+
+
+// express middleware to do some thing (verifying token/logging user activity etc) before reaching request to router
+
+app.use((req,res,next)=>{
+
+    console.log('.......Express Middleware invoked...')
+    console.log('req.method',req.method)
+    console.log('req.path',req.path)
+    next()
+
+/*    // below is the example of middleware uses
+    if(req.method==='GET'){
+        res.send('Get Request are disabled..')
+    }else{
+        next()
+    }*/
+
+   /* // below is the example of middleware uses
+    if(req.method==='GET' || req.method==='POST' || req.method==='PATCH' || req.method==='DELETE'){
+        res.status(503).send('Site is currently down !! come back soon.')
+    }else{
+        next()
+    }*/
+})
+
 app.use(express.json())
 app.use(userRouter);
 app.use(taskRouter)
-
 
 app.listen(port, () => {
     console.log(`-------- Server is up on port ${port} ---------`)
