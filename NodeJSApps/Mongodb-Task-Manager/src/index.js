d75b27c62f2cb3adbfdff910d6707af37bec204f
Sunil Kumar Maurya <sunilkumar.maurya@team.telstra.com>
2021-01-03 08:40:56 +0530

    JWT JsonWebToken part -1

--- NodeJSApps/Mongodb-Task-Manager/src/index.js
+++ NodeJSApps/Mongodb-Task-Manager/src/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 require('./db/mongoose')
-const userRouter =require('../src/router/user')
-const taskRouter =require('../src/router/task')
+const userRouter = require('../src/router/user')
+const taskRouter = require('../src/router/task')
 
 const port = process.env.PORT || 3000
 app.use(express.json())
@@ -14,15 +14,23 @@ app.listen(port, () => {
     console.log(`-------- Server is up on port ${port} ---------`)
 })
 
-const bcrypt = require('bcryptjs');
-const myfunciton =async ()=>{
-    const password='Test123!@#';
-    const bcryptPassword = await bcrypt.hash(password,8);
-    console.log('password ',password)
-    console.log('bcryptPassword ',bcryptPassword)
+const jwt = require('jsonwebtoken')
 
-    const isMatch =await bcrypt.compare('Test123!@#',bcryptPassword);
-    console.log('isMatch value ',isMatch)
+const myfunciton = async () => {
+    const token = jwt.sign({_di: 'GoldySunil'}, 'thisismynewcourse')
+    console.log('token', token)
+    //valid token validation scenario
+   const data =jwt.verify(token,'thisismynewcourse')
+    console.log(data);
+
+    //Invalid token validation scenario
+    const invalidata =jwt.verify(token,'InvalidSecrtedKey')
+    console.log(invalidata);
+    // output will be  UnhandledPromiseRejectionWarning: JsonWebTokenError: invalid signature
+
+    // set expiry time for JWT
+    const tokenWithExpriry = jwt.sign({_di: 'GoldySunil'}, 'thisismynewcourse',{expiresIn: '7 days'})
+    console.log('token', tokenWithExpriry)
 }
 myfunciton()
 
